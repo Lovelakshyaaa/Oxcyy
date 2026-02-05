2afa88b9d6cd28e5f6dca3b6bdf31ce3c6bb6c68
Lovelakshya1 <yogirajpurohit08@gmail.com>
2026-02-05 17:18:47 +0530

    Create clients.dart

---
+++ lib/clients.dart
@@ -0,0 +1,37 @@
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+
+// Credit: Based on Musify/yt-dlp implementation
+// These "Fake Clients" trick YouTube into giving us working streams.
+
+const customAndroidVr = YoutubeApiClient({
+  'context': {
+    'client': {
+      'clientName': 'ANDROID_VR',
+      'clientVersion': '1.65.10',
+      'deviceModel': 'Quest 3',
+      'osVersion': '12L',
+      'osName': 'Android',
+      'androidSdkVersion': '32',
+      'hl': 'en',
+      'timeZone': 'UTC',
+      'utcOffsetMinutes': 0,
+    },
+    'contextClientName': 28,
+    'requireJsPlayer': false,
+  },
+}, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
+
+const customAndroidSdkless = YoutubeApiClient({
+  'context': {
+    'client': {
+      'clientName': 'ANDROID',
+      'clientVersion': '20.10.38',
+      'userAgent':
+          'com.google.android.youtube/20.10.38 (Linux; U; Android 11) gzip',
+      'osName': 'Android',
+      'osVersion': '11',
+    },
+  },
+  'contextClientName': 3,
+  'requireJsPlayer': false,
+}, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
