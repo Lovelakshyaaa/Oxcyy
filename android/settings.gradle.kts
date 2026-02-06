25a8ee34df2dbdf47e86e26190f4ee32a1a8a539
Lovelakshya1 <yogirajpurohit08@gmail.com>
2026-02-06 21:39:18 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,8 +19,10 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    // Downgraded to 7.3.0 to allow old plugins
+    id("com.android.application") version "7.3.0" apply false
+    // Downgraded to 1.7.10 to match the old Android plugin
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
 
 include(":app")
