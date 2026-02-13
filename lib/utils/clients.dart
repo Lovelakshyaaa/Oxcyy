e758f80dc420f2ed4b265aaea10cf073f1956123
Lovelakshya1 <yogirajpurohit08@gmail.com>
2026-02-13 14:49:14 +0530

    Update clients.dart

--- lib/utils/clients.dart
+++ lib/utils/clients.dart
@@ -1,39 +1,34 @@
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
+import 'dart:io';
 
-// Credit: Based on Musify/yt-dlp implementation
-// These "Fake Clients" trick YouTube into giving us working streams.
+/// Creates a YoutubeHttpClient that mimics an Oculus Quest 3 (Android VR)
+YoutubeHttpClient createAndroidVrClient() {
+  final client = YoutubeHttpClient(
+    client: IOClient(
+      HttpClient()
+        ..userAgent = 'com.google.android.youtube/20.10.38 (Linux; U; Android 12L) gzip'
+        ..connectionTimeout = const Duration(seconds: 30),
+    ),
+  );
+  client.defaultHeaders.addAll({
+    'User-Agent': 'com.google.android.youtube/20.10.38 (Linux; U; Android 12L) gzip',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'X-YouTube-Client-Name': '28',   // ANDROID_VR
+    'X-YouTube-Client-Version': '1.65.10',
+  });
+  return client;
+}
 
-// The "Golden Key" - Mimics an Oculus Quest 3 to get high-quality M4A streams
-const customAndroidVr = YoutubeApiClient({
-  'context': {
-    'client': {
-      'clientName': 'ANDROID_VR',
-      'clientVersion': '1.65.10',
-      'deviceModel': 'Quest 3',
-      'osVersion': '12L',
-      'osName': 'Android',
-      'androidSdkVersion': '32',
-      'hl': 'en',
-      'timeZone': 'UTC',
-      'utcOffsetMinutes': 0,
-    },
-    'contextClientName': 28,
-    'requireJsPlayer': false,
-  },
-}, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
-
-// Backup Client - Mimics an older Android phone (use only if VR fails)
-const customAndroidSdkless = YoutubeApiClient({
-  'context': {
-    'client': {
-      'clientName': 'ANDROID',
-      'clientVersion': '20.10.38',
-      'userAgent':
-          'com.google.android.youtube/20.10.38 (Linux; U; Android 11) gzip',
-      'osName': 'Android',
-      'osVersion': '11',
-    },
-  },
-  'contextClientName': 3,
-  'requireJsPlayer': false,
-}, 'https://www.youtube.com/youtubei/v1/player?prettyPrint=false');
+/// Fallback Android client
+YoutubeHttpClient createAndroidClient() {
+  final client = YoutubeHttpClient();
+  client.defaultHeaders.addAll({
+    'User-Agent': 'com.google.android.youtube/20.10.38 (Linux; U; Android 11) gzip',
+    'X-YouTube-Client-Name': '3',    // ANDROID
+    'X-YouTube-Client-Version': '20.10.38',
+  });
+  return client;
+}
